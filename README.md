2af65980e80250b0c6a74d6ee58064ed17774d4e
phaesoo <phaesoo@gmail.com>
2021-03-21 23:36:56 +0900

    Update README

--- README.md
+++ README.md
@@ -3,3 +3,31 @@
 [![Build Status](https://travis-ci.com/phaesoo/shield.svg?branch=main)](https://travis-ci.com/phaesoo/shield)
 
 Shield is API key base authentication service
+
+# Getting started
+
+## Install dependencies
+```bash
+make deps
+```
+
+## Build
+```bash
+make main
+```
+
+## Run
+```bash
+./main
+```
+
+# Testing
+Unit test
+```
+make test
+```
+
+Integration test
+```
+make test-ci
+```
--- internal/services/keyauth/server.go
+++ internal/services/keyauth/server.go
@@ -6,6 +6,7 @@ import (
 	"net/http"
 
 	"github.com/go-chi/chi"
+	"github.com/phaesoo/shield/internal/request"
 	"github.com/phaesoo/shield/internal/resp"
 )
 
@@ -24,13 +25,26 @@ func NewServer(service *Service) *Server {
 }
 
 func (s *Server) RegisterRoutes(router *chi.Mux) {
-	router.Put(routeStatus, s.HandlePostVerify)
+	router.Put(routeStatus, s.HandleTokenVerify)
 }
 
-func (s *Server) HandlePostVerify(w http.ResponseWriter, r *http.Request) {
+func (s *Server) HandleTokenVerify(w http.ResponseWriter, r *http.Request) {
+	token := r.Header.Get("token")
+	if token != "" {
+		resp.BadRequest(w, resp.CodeInvalidRequest)
+		return
+	}
+
+	params := struct{
+		URLPath
+	 }
+
+
+	request.BindJSON(r, )
+
 	log.Print("HandlePostVerify called")
 
-	if _, err := s.service.Verify(context.Background(), "", "", ""); err != nil {
+	if _, err := s.service.Verify(context.Background(), token, "", ""); err != nil {
 		resp.Error(w, err, "")
 		return
 	}
